2f8fa9340b404fa12125889b664221072b4700a7
AbdulazizaAaA <abdulazizabdusamotov001@gmail.com>
2025-08-07 17:09:14 +0500

    abdulaziz

--- review/models.py
+++ review/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from course.models import Course
 
 class Review(models.Model):
-    first_name = models.CharField(max_length=200,blank=True,null=True)
-    last_name = models.CharField(max_length=200,blank=True,null=True)
-    text = models.CharField(max_length=200)
-    rayting = models.IntegerField()
+    coure = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course')
+    first_name = models.CharField(verbose_name="Familiya",max_length=200,blank=True,null=True)
+    last_name = models.CharField(verbose_name="Ism",max_length=200,blank=True,null=True)
+    text = models.CharField(verbose_name="review",max_length=200)
+    rayting = models.IntegerField(verbose_name="rayting")
