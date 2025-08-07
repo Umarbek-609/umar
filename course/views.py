72cf829d5804237cebfca27d17b98027f5e0d5d2
Amirhon386 <amirhonabduvaliyev@gmail.com>
2025-08-07 17:16:37 +0500

    course

--- course/views.py
+++ course/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets, permissions, filters
-from .models import Course
-from .serializer import CourseSerializer
-from utils.permissions import IsOwnerOrReadOnly
+from .models import Course,Lessons
+from .serializer import CourseSerializer,LessonSerializer
+from utils.permissions import IsOwnerOrReadOnly,IsCourseOwnerOrReadOnly
 from utils.pagination import CustomPagination
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -10,7 +14,19 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    pagination_class = [CustomPagination]
+    pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['title']
+    filterset_fields = ['created_at']
 
     def perform_create(self, serializer):
         serializer.save(instructor=self.request.user)
+
+
+class LessonViewSet(viewsets.ModelViewSet):
+    queryset = Lessons.objects.all()
+    serializer_class = LessonSerializer
+    permission_classes = [IsCourseOwnerOrReadOnly]
+    pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['title']
--- utils/permissions.py
+++ utils/permissions.py
@@ -9,6 +9,19 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.instructor == request.user
+    
+class IsCourseOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.course.instructor == request.user
+
+
+
 
 class IsRegistered(permissions.BasePermission):
     def has_permission(self, request, view):
