dbf488f60e7fca73946743e3f7dbcce08f2bdfed
Erkaboy0313 <fayyozbekov20@gmail.com>
2025-08-09 17:32:47 +0500

    s

--- user/views.py
+++ user/views.py
@@ -1,14 +1,18 @@
 from .serializer import RegisterSerializer,ProfileSerializer,ResetPasswordRequestSerializer,ResetPasswordSerializer
-from rest_framework.response import Response
-from .models import CustomUser
+from .models import CustomUser,PasswordReset
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics
-from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from user.models import CustomUser,PasswordReset
 from .serializer import PasswordResetRequestSerializer
 from rest_framework import status
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.conf import settings
 import os
 
 class RegisterView(ModelViewSet):
@@ -80,34 +84,16 @@ class ResetPassword(generics.GenericAPIView):
         else: 
             return Response({'error':'No user found'}, status=404)
 
-
-
-
-# views.py
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import get_user_model
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import send_mail
-from django.conf import settings
-
-User = get_user_model()
-
-
-
-class PasswordResetRequestView(APIView):
+class PasswordForgetRequestView(APIView):
     def post(self, request):
         serializer = PasswordResetRequestSerializer(data=request.data)
         if serializer.is_valid():
             email = serializer.validated_data['email']
             try:
-                user = User.objects.get(email=email)
+                user = CustomUser.objects.get(email=email)
                 uid = urlsafe_base64_encode(force_bytes(user.pk))
                 token = default_token_generator.make_token(user)
-                reset_link = f"http://127.0.0.1:8000/auth/password-reset-confirm/{uid}/{token}/"
+                reset_link = request.build_absolute_uri(f"/auth/password-reset-confirm/{uid}/{token}/")
                 # Send email
                 send_mail(
                     subject="Reset your password",
@@ -117,30 +103,16 @@ class PasswordResetRequestView(APIView):
                 )
 
                 return Response({"message": "Password reset link sent."}, status=status.HTTP_200_OK)
-            except User.DoesNotExist:
+            except CustomUser.DoesNotExist:
                 return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-
-# views.py
-
-from django.utils.http import urlsafe_base64_decode
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth import get_user_model
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-User = get_user_model()
-
-class PasswordResetConfirmView(APIView):
+class PasswordForgetConfirmView(APIView):
     def post(self, request, uidb64, token):
         try:
             uid = urlsafe_base64_decode(uidb64).decode()
-            user = User.objects.get(pk=uid)
-        except (User.DoesNotExist, ValueError, TypeError, OverflowError):
+            user = CustomUser.objects.get(pk=uid)
+        except (CustomUser.DoesNotExist, ValueError, TypeError, OverflowError):
             return Response({'error': 'Noto‘g‘ri token yoki foydalanuvchi.'}, status=status.HTTP_400_BAD_REQUEST)
 
         if default_token_generator.check_token(user, token):
