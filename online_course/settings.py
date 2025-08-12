19b032edcdae2513bfac518f2504e7a3a7c39bd9
Umarbek-609 <umarbekumarbekumarbek777@gmail.com>
2025-08-12 15:35:46 +0500

    wde

--- online_course/settings.py
+++ online_course/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG"))
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
